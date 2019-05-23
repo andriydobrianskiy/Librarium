26a6c5ca5563ccdcdd587535e3f216b79cb84fe0
Volodymyr Oseredchuk <oseredchuk.v@gmail.com>
2019-05-23 16:32:36 +0300

    Create book service

---
+++ src/main/java/com/softserve/academy/service/BookService.java
@@ -0,0 +1,57 @@
+package com.softserve.academy.service;
+
+import com.softserve.academy.Entity.Book;
+import com.softserve.academy.Entity.User;
+import com.softserve.academy.dao.BookDao;
+import com.softserve.academy.dao.BookDaoImpl;
+import org.apache.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookService {
+    private static final Logger LOGGER = Logger.getLogger(BookService.class);
+    private static final BookDao bookDao = new BookDaoImpl();
+
+    public List<Book> getAllBooks() {
+        return bookDao.getAllBooks();
+    }
+
+    public List<Book> getAllBooksByUser(User user) throws IllegalArgumentException {
+        if (user == null) {
+            throw new IllegalArgumentException("User is null");
+        } else if (user.getId() <= 0) {
+            throw new IllegalArgumentException("User ID is not valid");
+        }
+        return bookDao.getAllBooksByUser(user);
+    }
+
+    public boolean insertBook(Book book) throws IllegalArgumentException {
+        if (book == null) {
+            throw new IllegalArgumentException("Book is null");
+        }
+        if (bookDao.exists(book)) {
+            return false;
+        } else {
+            return bookDao.insertBook(book);
+        }
+    }
+
+    public int getCountOfBookOrders(Book book) throws IllegalArgumentException {
+        if (book == null) {
+            throw new IllegalArgumentException("Book is null");
+        } else if (book.getId() <= 0) {
+            throw new IllegalArgumentException("Book ID is not valid");
+        }
+        return bookDao.getCountOfBookOrdersByBookId(book.getId());
+    }
+
+    public int getAverageTimeOfReading(Book book) throws IllegalArgumentException {
+        if (book == null) {
+            throw new IllegalArgumentException("Book is null");
+        } else if (book.getId() <= 0) {
+            throw new IllegalArgumentException("Book ID is not valid");
+        }
+        return bookDao.getAverageTimeOfReadingByBookId(book.getId());
+    }
+}
