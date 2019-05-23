45a55cfdfef8c29f3271697f7714255bd1be903c
Volodymyr Oseredchuk <oseredchuk.v@gmail.com>
2019-05-23 16:25:01 +0300

    Create methods for getting all books and checking for existing

--- src/main/java/com/softserve/academy/dao/BookDao.java
+++ src/main/java/com/softserve/academy/dao/BookDao.java
@@ -7,7 +7,8 @@ import java.util.List;
 
 public interface BookDao {
     List<Book> getAllBooksByUser(User user);
-
+    List<Book> getAllBooks();
+    boolean exists(Book book);
     boolean insertBook(Book book);
     int getCountOfBookOrdersByBookId(int bookId);
     int getAverageTimeOfReadingByBookId(int bookId);
