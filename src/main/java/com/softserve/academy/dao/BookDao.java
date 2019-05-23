cc6a968b55f0c2b30f8a43240055f27cdb0d3a2e
OlhaLozinska <olja.lviv133@gmail.com>
2019-05-23 16:07:27 +0300

    add method for ordering books (ASC/DESC) by Period

--- src/main/java/com/softserve/academy/dao/BookDao.java
+++ src/main/java/com/softserve/academy/dao/BookDao.java
@@ -3,12 +3,18 @@ package com.softserve.academy.dao;
 import com.softserve.academy.Entity.Book;
 import com.softserve.academy.Entity.User;
 
+import java.sql.Date;
 import java.util.List;
+import java.util.Map;
 
 public interface BookDao {
     List<Book> getAllBooksByUser(User user);
 
+    Map<Book, Integer> getOrderedListOfBooksInPeriod(Date startDate, Date endDate, boolean sortAsc);
+
     boolean insertBook(Book book);
+
     int getCountOfBookOrdersByBookId(int bookId);
+
     int getAverageTimeOfReadingByBookId(int bookId);
 }
