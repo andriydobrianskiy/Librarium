7e71c7d3b584a51ef7afea9df7905e29da2c93b7
Volodymyr Oseredchuk <oseredchuk.v@gmail.com>
2019-05-25 00:35:18 +0300

    create simple books view

--- src/main/java/com/softserve/academy/controller/BooksServlet.java
+++ src/main/java/com/softserve/academy/controller/BooksServlet.java
@@ -1,5 +1,11 @@
 package com.softserve.academy.controller;
 
+import com.softserve.academy.Entity.Book;
+import com.softserve.academy.connectDatabase.DBConnection;
+import com.softserve.academy.service.BookService;
+import com.softserve.academy.service.BookServiceImpl;
+import org.apache.log4j.Logger;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -7,15 +13,28 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @WebServlet(name = "BooksServlet", urlPatterns = {"/books"})
 public class BooksServlet extends HttpServlet {
+    private static final Logger LOGGER = Logger.getLogger(BooksServlet.class);
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        RequestDispatcher rd = this.getServletContext().getRequestDispatcher("/WEB-INF/pages/books.jsp");
-        rd.forward(request,response);
+        DBConnection dbConnection = new DBConnection();
+        dbConnection.connect();
+        List<Book> bookList = new BookServiceImpl().getAllBooks();
+        Map<Book, String> books = new HashMap<>();
+        for (int i = 0; i < bookList.size(); i++) {
+           books.put(bookList.get(i), "photo" + (i + 1));
+        }
+        request.setAttribute("books", books);
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/pages/books.jsp");
+        rd.forward(request, response);
     }
 }
--- src/main/webapp/WEB-INF/pages/books.jsp
+++ src/main/webapp/WEB-INF/pages/books.jsp
@@ -1,16 +1,34 @@
-<%@ taglib uri = "http://java.sun.com/jsp/jstl/core" prefix = "c" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib uri = "http://java.sun.com/jsp/jstl/core" prefix = "c" %>
+
 <html>
 <head>
     <title>Books</title>
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <!-- Latest compiled JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
     <!-- jQuery library -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </head>
 <body>
-<h2>Books page</h2>
+<div class="container-fluid">
+    <div class="row">
+        <c:forEach items="${books}" var="book">
+            <div class = "col-md-3 d-flex" >
+                <div class="card flex-fill">
+                    <img class="card-img-top" src="${pageContext.request.contextPath}/images/${book.value}.jpg" alt="Card image">
+                    <div class="card-body">
+                        <h4 class="card-title"><c:out value="${book.key.name}"/></h4>
+                        <p class="card-text">Author</p>
+                    </div>
+                    <div class="card-footer">
+                        <a href="#" class="btn btn-primary stretched-link">See detailed info</a>
+                    </div>
+                </div>
+            </div>
+        </c:forEach>
+    </div>
+</div>
 </body>
 </html>
